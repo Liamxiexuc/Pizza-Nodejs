75be62a1291ea9525c3f4005deba440e274e566f
unknown <liachenxiexu@gmail.com>
2019-11-20 16:50:46 +1100

    order.js done

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -1,28 +1,88 @@
 const Order = require("../models/order");
 
 async function addOrder(req, res) {
+  const {
+    orderStatus,
+    orderTotalPrice,
+    payStatus,
+    receiverAddress,
+    receiverName,
+    receiverPhone
+  } = req.body;
+
   const order = new Order({
-    orderStatus: "processing",
-    orderTotalPrice: "123",
-    payStatus: "Paid",
-    receiverAddress: "12 qwee St",
-    receiverName: "James",
-    receiverPhone: "0404380038"
+    orderStatus,
+    orderTotalPrice,
+    payStatus,
+    receiverAddress,
+    receiverName,
+    receiverPhone
   });
   await order.save();
   return res.json(order);
 }
 
-function getOrder(req, res) {}
+async function getOrder(req, res) {
+  const { id } = req.params;
+
+  const order = await Order.findById(id);
+
+  if (!order) {
+    return res.status(404).json("order not found");
+  }
+
+  return res.json(order);
+}
 
 async function getAllOrders(req, res) {
   const orders = await Order.find();
   return res.json(orders);
 }
 
-function updateOrder(req, res) {}
+async function updateOrder(req, res) {
+  const { id } = req.params;
 
-function deleteOrder(req, res) {}
+  const {
+    orderStatus,
+    orderTotalPrice,
+    payStatus,
+    receiverAddress,
+    receiverName,
+    receiverPhone
+  } = req.body;
+
+  const newOrder = await Order.findByIdAndUpdate(
+    id,
+    {
+      orderStatus,
+      orderTotalPrice,
+      payStatus,
+      receiverAddress,
+      receiverName,
+      receiverPhone
+    },
+    {
+      new: true
+    }
+  );
+
+  if (!newOrder) {
+    return res.status(404).json("order not found");
+  }
+
+  return res.json(newOrder);
+}
+
+async function deleteOrder(req, res) {
+  const { id } = req.params;
+
+  const order = await Order.findByIdAndDelete(id);
+
+  if (!order) {
+    return res.status(404).json("order not found");
+  }
+  return res.sendStatus(200);
+}
 
 module.exports = {
   addOrder,
