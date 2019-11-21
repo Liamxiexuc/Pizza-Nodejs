a09031b0a112162e840ba1776a6a7ae8a081a3b9
JimHui12 <jianghua.hui@uq.net.au>
2019-11-21 12:20:36 +1000

    dish.js updated

--- src/controllers/dish.js
+++ src/controllers/dish.js
@@ -2,6 +2,7 @@ const Dish = require("../models/dish");
 
 async function addDish(req, res) {
     const {
+        id,
         productName,
         price,
         productType,
@@ -13,19 +14,84 @@ async function addDish(req, res) {
 
     const existDish = await Dish.findById(id);
     if(existDish) {
-        
+        return res.status(400).json('dish exist');
     }
+
+    const dish = new Dish ({
+        id,
+        productName,
+        price,
+        productType,
+        productInfo,
+        productionDiscount,
+        photo,
+        category
+    });
+    await dish.save();
+    return res.json(dish);
 }
 
-function getDish(req, res) {}
+async function getDish(req, res) {
+    const {id} = req.params;
 
-function getAllDishes(req, res) {
-    res.json('hello from all dishes');
+    const dish = await Dish.findById(id);
+    if(!dish) {
+        return res.status(404).json('dish not found');
+    }
+    return res.json(dish);
 }
 
-function updateDish(req, res) {}
+async function getAllDishes(req, res) {
+    const dishs = await Dish.find();
+    return res.json(dishs);
+}
 
-function deleteDish(req, res) {}
+async function updateDish(req, res) {
+    const { id } = req.params;
+
+    const {
+        id,
+        productName,
+        price,
+        productType,
+        productInfo,
+        productionDiscount,
+        photo,
+        category        
+    } = req.body;
+
+    const newDish = await Dish.findByIdAndUpdate(
+        id,
+        {
+            productName,
+            price,
+            productType,
+            productInfo,
+            productionDiscount,
+            photo,
+            category 
+        },
+        {
+            new: true
+        }
+    );
+
+    if (!newDish) {
+        return res.status(404).json("dish not found");
+    }
+
+    return res.json(newDish);
+}
+
+async function deleteDish(req, res) {
+    const { id } = req.params;
+    const dish = await Dish.findByIdAndDelete(id);
+
+    if(!dish) {
+        return res.status(404).json("dish not found");
+    }
+    return res.sendStatus(200);
+}
 
 module.exports = {
     addDish,
